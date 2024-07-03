3d87d6758f94605c789bd5e4089ff23069a601e5
AmishMaqbool <amishmaqbool@gmail.com>
2024-07-04 01:57:40 +0500

    Sidebar Responsive fix

--- src/components/common/HomeNavbar.js
+++ src/components/common/HomeNavbar.js
@@ -6,11 +6,11 @@ const HomeNavbar = () => {
     <div className="bg-background border-b border-line-color">
       <div className="px-2 max-md:px-0 container mx-auto flex justify-between items-center py-[22.5px]">
         <a className="" href="/">
-          <img className="w-[106px] h-[19px]" src={Logo} alt="Logo"/>
+          <img className="max-sm:ml-[10px] w-[106px] h-[19px]" src={Logo} alt="Logo"/>
         </a>
         <div className="flex gap-6 items-center">
           <p className="font-normal text-xl text-dark-blue hidden sm:block">Currently in private beta, invite-only. </p>
-          <p className="font-normal text-xl text-description-color cursor-pointer">Login</p>
+          <p className="max-sm:mr-[10px] font-normal text-xl text-description-color cursor-pointer">Login</p>
         </div>
       </div>
     </div>
