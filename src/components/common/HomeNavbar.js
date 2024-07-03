fe068a26d1d86b5b93cc44ffef0548b75352fa13
AmishMaqbool <amishmaqbool@gmail.com>
2024-07-04 01:59:44 +0500

    Navbar Responsive fix

--- src/components/common/HomeNavbar.js
+++ src/components/common/HomeNavbar.js
@@ -9,7 +9,7 @@ const HomeNavbar = () => {
           <img className="max-sm:ml-[10px] w-[106px] h-[19px]" src={Logo} alt="Logo"/>
         </a>
         <div className="flex gap-6 items-center">
-          <p className="font-normal text-xl text-dark-blue hidden sm:block">Currently in private beta, invite-only. </p>
+          <p className="font-normal text-xl text-dark-blue hidden sm:block">Currently in private beta, invite-only.</p>
           <p className="max-sm:mr-[10px] font-normal text-xl text-description-color cursor-pointer">Login</p>
         </div>
       </div>
