d73eb9adc34f5c85b5e555e65ec9b8ceba7f3503
Amishmaqbool <amishmaqbool@gmail.com>
2024-07-21 00:23:59 +0500

    made details responsive

--- src/pages/MarketPlace.js
+++ src/pages/MarketPlace.js
@@ -13,7 +13,7 @@ const MarketPlace = ({ onCardSelect }) => {
 
   return (
     <div className="max-md:bg-[#13121c] w-[100%] flex gap-0 md:gap-0 flex-col md:flex-row rounded-[10px] max-md:h-full overflow-hidden">
-      <div className="w-full md:w-[25%] sticky top-0 z-10 bg-[#13121c] max-md:max-h-[120px] max-md:h-[120px] overflow-y-auto overflow-x-hidden scrollbar">
+      <div className="w-full md:w-[25%] sticky top-0 z-10 bg-[#13121c] max-md:max-h-[120px] max-md:h-[120px] overflow-hidden scrollbar">
         <TemplateLeftSidebar handleMenuClick={handleMenuClick} />
       </div>
       <div className="bg-[#13121c] max-xl:px-4 mt-2 max-md:max-h-[calc(100vh-300px)] max-h-[80vh] md:h-[80vh] w-full md:w-[75%] overflow-y-auto overflow-hidden scrollbar">
