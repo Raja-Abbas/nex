306868ad1f1cf868f347166c8d82963faab40365
Amishmaqbool <amishmaqbool@gmail.com>
2024-07-20 20:47:06 +0500

    made marketplace responsive

--- src/components/common/MarketplaceCardsContent.js
+++ src/components/common/MarketplaceCardsContent.js
@@ -22,21 +22,13 @@ export default function CardsContent({ selectedMenu, onCardSelect }) {
   };
 
   return (
-    <div className="px-0 md:px-5 py-5 grid gap-[16px] max-[470px]:grid-cols-1 grid-cols-2 lg:grid-cols-3">
+    <div className="px-0 md:px-5 py-5 grid gap-[16px] max-[470px]:grid-cols-1 grid-cols-2 xl:grid-cols-3">
       {filteredData.map((item, index) => (
         <div
           key={index}
           className="relative px-8 pr-6 font-semibold py-5 rounded-[5px] shadow-2xl border border-light-gray border-opacity-20 bg-[#171621]"
         >
           <div>
-            {item.title === "AnythingLLM" && (
-              <img
-                src={Link}
-                alt="Link"
-                className="absolute right-2 top-2 w-[17px] h-[15px] cursor-pointer"
-                onClick={() => handleLinkClick(item.title)}
-              />
-            )}
             <img className="min-h-[40px] max-h-[40px]" src={item.logo} alt={item.logo} />
             <p className="pt-3 font-normal max-w-[310px] text-2xl text-white">{item.title}</p>
             <p className="min-h-[90px] max-w-[310px] pt-[5px] font-normal text-xl text-description-color">
