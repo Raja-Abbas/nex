f51c5b448cd73a02cdc2cab8d04aee7bc87aac47
Amishmaqbool <amishmaqbool@gmail.com>
2024-07-25 11:43:07 +0500

    minor fixes

--- src/components/common/404.js
+++ src/components/common/404.js
@@ -3,8 +3,8 @@ import HomeSvg from '../../assets/svgs/homeSvg.svg';
 
 const BrokenPage = () => {
   return (
-    <div className=''>
-      <div className='flex flex-col gap-5 text-white overflow-hidden items-center justify-center min-h-screen'>
+    <div className='flex'>
+      <div className='flex flex-col gap-5 text-white overflow-hidden items-center justify-center'>
       <img src={HomeSvg} alt="HomeSvg" />
       <div className='text-center'>
         <h1 className='text-4xl font-bold mb-4'>
