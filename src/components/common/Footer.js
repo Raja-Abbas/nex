3c1dd752fad76c288763648c7dc84d04bca79205
Amishmaqbool <amishmaqbool@gmail.com>
2024-08-17 01:02:21 +0500

    updated scroll

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-background py-[21.5px] border-t border-line-color z-[1000]">
+    <footer className="bg-background py-[21.5px] border-t border-line-color z-[10]">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center px-2">
         <p className="font-normal text-base text-description-color">NexLayer - Launch, Scale, and Conquer with AI</p>
         <div className="flex gap-4 md:place-self-center md:justify-self-end">
