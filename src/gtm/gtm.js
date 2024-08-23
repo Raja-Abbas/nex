f385cb38fa993e8a517ddcd26706d789a33d736e
Amishmaqbool <amishmaqbool@gmail.com>
2024-08-23 18:19:18 +0500

    gtm module integrated

--- src/gtm/gtm.js
+++ src/gtm/gtm.js
@@ -1,6 +1,11 @@
 // gtm.js
-import TagManager from "react-gtm-module";
-const gtmId = "GTM-NP6HXJBZ";
+import TagManager from 'react-gtm-module';
+
+const gtmId = process.env.REACT_APP_GTM_ID;
+
+if (!gtmId) {
+  console.error('GTM_ID is not defined in environment variables');
+}
 
 export const initializeTagManager = () => {
   TagManager.initialize({ gtmId });
