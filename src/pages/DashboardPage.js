04ab312694c5f6131650eca6b2ef212a4e44b325
Amishmaqbool <amishmaqbool@gmail.com>
2024-08-25 02:05:49 +0500

    template logos updated

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import Globe from "../assets/svgs/globe.svg";
 import { deploymentData } from "../constants/Framework";
 import { useCardTitle } from "../context/CardTitleContext";
@@ -7,12 +7,10 @@ import { useCredit } from "../context/CreditContext";
 import ChatBotIcon from "../components/ChatBotIcon";
 import { useSelector } from "react-redux";
 
-
-
 function DashboardPage({ selectedCard }) {
   const { cardTitle } = useCardTitle();
   const { credit } = useCredit();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const { namespace } = useSelector((state) => state.deployment);
 
   const cardToDisplay = selectedCard || {
@@ -20,54 +18,57 @@ function DashboardPage({ selectedCard }) {
   };
 
   const handleReferFriend = () => {
-    navigate('/dashboard/refer-a-friend');
+    navigate("/dashboard/refer-a-friend");
   };
 
   return (
     <div className="grid max-lg:px-4 w-full scrollbar overflow-x-hidden gap-10 justify-center max-lg:grid-cols-1 max-lg:grid-rows-2 pt-20">
       <div className="max-w-[1200px] flex flex-col gap-10">
-      <div className="relative w-full lg:py-3 xl:h-[56px] flex items-center max-md:px-2 md:px-6 bg-[black] text-white border border-[#3D3F40] rounded-[7px]">
-        <p className="font-normal max-md:px-1 max-md:text-base md:text-lg rounded-lg py-1 md:px-3 text-dark-blue">
-          Exclusive nexlayer credits{" "}
-          <span className="max-md:text-[10px] md:text-xl font-thin mx-5"> | </span>{" "}
-          ${credit.toFixed(0)}
-        </p>
-        <button
-          onClick={handleReferFriend} // Update click handler
-          className="z-20 text-black absolute end-1 bg-light-blue hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md max-md:text-xs md:text-lg px-4 lg:py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Refer a Friend
-        </button>
-      </div>
-      <div className="grid max-md:grid-cols-1 max-lg:grid-cols-2 lg:grid-cols-3 lg:grid-rows-1 pb-10">
-        <div className="bg-card-color flex flex-col justify-center items-start gap-3 max-md:text-nowrap max-md:w-[280px] md:w-[380px] border border-[#32474C] relative rounded-lg py-4 size-fit max-md:px-2 md:px-6 shadow-xl">
-          <div className="flex w-full justify-between">
-            <div className="text-lg text-white tracking-0 font-[300]">
-              {cardToDisplay.title} Application
-            </div>
-            <div>
-              <a
-                href="/build"
-                className="px-[10px] py-[4.5px] font-normal text-sm rounded-full bg-light-blue text-[black]"
-              >
-                Redeploy
-              </a>
-            </div>
-          </div>
-          <a
-            href={deploymentData.url}
-            className="flex gap-2 items-center"
-            target="_blank"
-            rel="noreferrer"
+        <div className="relative w-full lg:py-3 xl:h-[56px] flex items-center max-md:px-2 md:px-6 bg-[black] text-white border border-[#3D3F40] rounded-[7px]">
+          <p className="font-normal max-md:px-1 max-md:text-base md:text-lg rounded-lg py-1 md:px-3 text-dark-blue">
+            Exclusive nexlayer credits{" "}
+            <span className="max-md:text-[10px] md:text-xl font-thin mx-5">
+              {" "}
+              |{" "}
+            </span>{" "}
+            ${credit.toFixed(0)}
+          </p>
+          <button
+            onClick={handleReferFriend} // Update click handler
+            className="z-20 text-black absolute end-1 bg-light-blue hover:bg-opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md max-md:text-xs md:text-lg px-4 lg:py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <img src={Globe} alt="Globe" className="w-4 h-5" />
-            <p className="text-base text-dark-blue leading-[24px] font-normal">
-                https://{namespace}.{cardToDisplay.slug}.alpha.nexlayer.ai
-            </p>
-          </a>
-          <p className="text-base text-white leading-[24px]">1 service </p>
+            Refer a Friend
+          </button>
         </div>
-      </div>
+        <div className="grid max-md:grid-cols-1 max-lg:grid-cols-2 lg:grid-cols-3 lg:grid-rows-1 pb-10">
+          <div className="bg-card-color flex flex-col justify-center items-start gap-3 max-md:text-nowrap max-md:w-[280px] md:w-[380px] border border-[#32474C] relative rounded-lg py-4 size-fit max-md:px-2 md:px-6 shadow-xl">
+            <div className="flex w-full justify-between">
+              <div className="text-lg text-white tracking-0 font-[300]">
+                {cardToDisplay.title} Application
+              </div>
+              <div>
+                <a
+                  href="/build"
+                  className="px-[10px] py-[4.5px] font-normal text-sm rounded-full bg-light-blue text-[black]"
+                >
+                  Redeploy
+                </a>
+              </div>
+            </div>
+            <a
+              href={deploymentData.url}
+              className="flex gap-2 items-center"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={Globe} alt="Globe" className="w-4 h-5" />
+              <p className="text-base text-dark-blue leading-[24px] font-normal">
+                https://{namespace}.{cardToDisplay.slug}.alpha.nexlayer.ai
+              </p>
+            </a>
+            <p className="text-base text-white leading-[24px]">1 service </p>
+          </div>
+        </div>
       </div>
       <ChatBotIcon />
     </div>
