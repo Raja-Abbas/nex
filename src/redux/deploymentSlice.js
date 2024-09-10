69bb7f87596a8a85e5c2fa002e1bb19e7cede8fe
Amishmaqbool <amishmaqbool@gmail.com>
2024-09-11 02:53:19 +0500

    metabase template added

--- src/redux/deploymentSlice.js
+++ src/redux/deploymentSlice.js
@@ -43,7 +43,7 @@ export const fetchDeploymentData = createAsyncThunk(
       const startTime = new Date().toISOString();
 
       const response = await fetchWithRetry(
-        `/startTemplateDeployment/${templateID}`,
+        `${SOCKET_SERVER_URL}/startTemplateDeployment/${templateID}`,
         {
           method: "POST",
           headers: {
@@ -92,7 +92,7 @@ export const fetchLogsData = createAsyncThunk(
       const previousTemplateID = Cookies.get("templateID");
 
       const response = await fetchWithRetry(
-        `/getDeploymentLogs/${namespace}/${templateID}`,
+        `${SOCKET_SERVER_URL}/getDeploymentLogs/${namespace}/${templateID}`,
         {
           method: "POST",
           headers: {
